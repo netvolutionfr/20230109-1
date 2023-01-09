c3d4f4c0ec788f2ead3b6073c35f3cf67d165051
stanis <stanis@netvolution.fr>
2023-01-09 14:57:47 +0100

    Restructuration de l'objet et des méthodes (notamment affichage)

--- index.php
+++ index.php
@@ -7,6 +7,27 @@ Class Utilisateur
     public int $annee_naissance = 0;
     public int $score = 0;
     public string $ville = "";
+
+    public function age()
+    {
+        return date("Y") - $this->annee_naissance;
+    }
+
+    public function afficher()
+    {
+        $age = $this->age();
+
+        echo "<div class=\"utilisateur\">
+            <h1>Utilisateur</h1>
+            <p>Nom : $this->nom</p>
+            <p>Prénom : $this->prenom</p>
+            <p>Ville : $this->ville</p>
+            <p>Mail : $this->email</p>
+            <p>Score : $this->score</p>
+            <p>Age : $age ans</p>
+        </div>";
+
+    }
 }
 
 
@@ -18,15 +39,5 @@ $utilisateur->email = "toto@titi.com";
 $utilisateur->annee_naissance = 1980;
 $utilisateur->score = 100;
 $utilisateur->ville = "Paris";
-$annee = date("Y");
-$age = $annee - $utilisateur->annee_naissance;
 
-echo "<div class=\"utilisateur\">
-    <h1>Utilisateur</h1>
-    <p>Nom : $utilisateur->nom</p>
-    <p>Prénom : $utilisateur->prenom</p>
-    <p>Ville : $utilisateur->ville</p>
-    <p>Mail : $utilisateur->email</p>
-    <p>Score : $utilisateur->score</p>
-    <p>Age : $age ans</p>
-</div>";
+$utilisateur->afficher();
